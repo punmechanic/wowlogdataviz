5a90321c3c907dafdf6599346f66ef455c47d9d2
Dan <dan@aredherring.tech>
2023-04-10 10:58:58 -0700

    add skeleton around log reading

--- csvify/main.rs
+++ csvify/main.rs
@@ -14,18 +14,53 @@ fn do_file(r: impl std::io::Read, w: impl std::io::Write) -> std::io::Result<()>
     todo!()
 }
 
+struct LogReader<R>(R)
+where
+    R: io::Read;
+
+struct Lines;
+
+impl<R> LogReader<R>
+where
+    R: io::Read,
+{
+    fn new(reader: R) -> Self {
+        todo!();
+    }
+
+    fn lines(&self) -> Lines {
+        todo!();
+    }
+}
+
+impl Iterator for Lines {
+    type Item = Result<String, io::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!()
+    }
+}
+
 fn count_max_columns(r: impl std::io::Read) -> io::Result<usize> {
-    BufReader::new(r).lines().try_fold(0, |prev, line| {
-        // There's one extra column because the first column will be both timestamp and event.
-        // TODO: I expect this could be much faster if we counted the incidences of , in the string rather than splitting it.
-        let n = line?.split(',').count() + 1;
-        Ok(if prev > n { prev } else { n })
-    })
+    LogReader::new(BufReader::new(r))
+        .lines()
+        .try_fold(0, |prev, line| {
+            // There's one extra column because the first column will be both timestamp and event.
+            // TODO: I expect this could be much faster if we counted the incidences of , in the string rather than splitting it.
+            let line = line?;
+            let n = line.split(',').count() + 1;
+            if n > 400 {
+                println!("{}", line);
+            }
+
+            Ok(if prev > n { prev } else { n })
+        })
 }
 
 fn main() {
     let out = stdout();
     let args = std::env::args().skip(1);
+    // Open files once to count the maximum number of columns; This is used to pad column count.
     let max_columns: std::io::Result<usize> =
         std::env::args()
             .skip(1)
@@ -37,7 +72,6 @@ fn main() {
 
     println!("max cols: {:?}", max_columns);
 
-    // Open files once to count the maximum number of columns.
     // for name in args {
     //     if let Err(error) = File::open(&name).and_then(|mut handle| do_file(&mut handle, &out)) {
     //         eprintln!("could not process {}: {}", name, error);
